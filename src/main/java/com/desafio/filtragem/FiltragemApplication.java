b86aefcb9f5c983f502c060edb389a40b63c514e
Rafyy2102 <rafpromais@yahoo.com.br>
2025-03-28 01:57:45 -0300

    feat: Criando backend

---
+++ src/main/java/com/desafio/filtragem/FiltragemApplication.java
@@ -0,0 +1,13 @@
+package com.desafio.filtragem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiltragemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiltragemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/desafio/filtragem/FiltragemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafio.filtragem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiltragemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
